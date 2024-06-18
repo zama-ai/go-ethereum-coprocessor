05bca5cec54ac67398142055bcb645bfad179cff
David Kazlauskas <deividas.kazlauskas@zama.ai>
2024-06-18 09:00:24 +0300

    Download keys via HTTP as alternative to files

--- scripts/run-single-node-devnet.sh
+++ scripts/run-single-node-devnet.sh
@@ -4,6 +4,9 @@ ACL_CONTRACT_ADDRESS='0x168813841d158Ea8508f91f71aF338e4cB4d396e'
 COPROCESSOR_CONTRACT_ADDRESS='0x6819e3aDc437fAf9D533490eD3a7552493fCE3B1'
 COPROCESSOR_ACCOUNT_ADDRESS='0xc9990FEfE0c27D31D0C2aa36196b085c0c4d456c'
 
+# serve keys so fhevm-go-coproc library can download at initialization
+python3 -m http.server -d /usr/share/devnet-resources/fhevm-keys 8000 &
+
 geth init --datadir /val-data /usr/share/devnet-resources/genesis.json
 geth init --datadir /rpc-data /usr/share/devnet-resources/genesis.json
 
@@ -12,6 +15,9 @@ bootnode -nodekey /usr/share/devnet-resources/boot.key -addr :30305 &
 
 echo Running RPC node
 FHEVM_GO_INIT_CKS=1 \
+	FHEVM_GO_SKS_URL=http://127.0.0.1:8000/sks \
+	FHEVM_GO_PKS_URL=http://127.0.0.1:8000/pks \
+	FHEVM_GO_CKS_URL=http://127.0.0.1:8000/cks \
 	FORCE_TRANSIENT_STORAGE=true \
 	FHEVM_GO_KEYS_DIR=/usr/share/devnet-resources/fhevm-keys \
 	FHEVM_CIPHERTEXTS_DB=/rpc-data/fhevm_ciphertexts.sqlite \
