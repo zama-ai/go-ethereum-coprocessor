a79ddcce045f87bf88628dca2901603cc2f44dd8
David Kazlauskas <deividas.kazlauskas@zama.ai>
2024-09-16 09:16:55 +0300

    Remove unneeded devnode dependencies

--- scripts/run-single-node-devnet.sh
+++ scripts/run-single-node-devnet.sh
@@ -6,9 +6,6 @@ ACL_CONTRACT_ADDRESS=${ACL_CONTRACT_ADDRESS:-0x168813841d158Ea8508f91f71aF338e4c
 COPROCESSOR_CONTRACT_ADDRESS=${COPROCESSOR_CONTRACT_ADDRESS:-0x6819e3aDc437fAf9D533490eD3a7552493fCE3B1}
 COPROCESSOR_ACCOUNT_ADDRESS=${COPROCESSOR_ACCOUNT_ADDRESS:-0xc9990FEfE0c27D31D0C2aa36196b085c0c4d456c}
 
-# serve keys so fhevm-go-coproc library can download at initialization
-nohup python3 -m http.server -d /usr/share/devnet-resources/fhevm-keys 8000 > /var/log/http-server.log &
-
 prysm-ctl testnet generate-genesis --fork=capella --num-validators=64 --genesis-time-delay=5 \
 	--output-ssz /consensus-genesis.ssz --chain-config-file=/usr/share/devnet-resources/consensus-config.yml \
 	--geth-genesis-json-in=/usr/share/devnet-resources/genesis.json \
@@ -98,15 +95,8 @@ nohup prysm-beacon --datadir=$NODE_DIR/consensus/beacondata \
   --force-clear-db > /var/log/rpc-beacon.log &
 
 echo Running RPC node execution
-FHEVM_GO_INIT_CKS=1 \
-  FHEVM_GO_SKS_URL=http://127.0.0.1:8000/sks \
-  FHEVM_GO_PKS_URL=http://127.0.0.1:8000/pks \
-  FHEVM_GO_CKS_URL=http://127.0.0.1:8000/cks \
-  FORCE_TRANSIENT_STORAGE=true \
-  FHEVM_GO_KEYS_DIR=/usr/share/devnet-resources/fhevm-keys \
   FHEVM_CIPHERTEXTS_DB=$NODE_DIR/fhevm_ciphertexts.sqlite \
   FHEVM_CONTRACT_ADDRESS=$COPROCESSOR_CONTRACT_ADDRESS \
-  FHEVM_COPROCESSOR_PRIVATE_KEY_FILE=$NODE_DIR/coprocessor.key \
   FORCE_TRANSIENT_STORAGE=true \
     geth --datadir $NODE_DIR --port 30308 --http --http.corsdomain='*' --http.addr 0.0.0.0 --http.port 8545 \
     --bootnodes 'enode://0b7b41ca480f0ef4e1b9fa7323c3ece8ed42cb161eef5bf580c737fe2f33787de25a0c212c0ac7fdb429216baa3342c9b5493bd03122527ffb4c8c114d87f0a6@127.0.0.1:0?discport=30305' \
